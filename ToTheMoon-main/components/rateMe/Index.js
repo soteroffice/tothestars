5146bf19fe8d6f541559f0f3de819cd02f882173
soteroffice <114199213+soteroffice@users.noreply.github.com>
2025-03-21 13:02:06 -0600

    Update Index.js

--- ToTheMoon-main/components/rateMe/Index.js
+++ ToTheMoon-main/components/rateMe/Index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { TiStar } from "react-icons/ti";
-import Fade from "react-reveal/Fade";
 import { IoArrowBack } from "react-icons/io5";
 import * as gtag from "@/lib/gtag";
 
@@ -64,73 +63,64 @@ function Index({ businessData = {} }) {
       <div className="w-full overflow-hidden min-h-screen bg-white relative">
         <div className={bgFadeClass}></div>
         {screen == "init" && (
-          <Fade when={screen == "init"} collapse unmountOnExit mountOnEnter>
-            <div className="absolute w-full items-center flex flex-col">
-              <Fade>
-                <img
-                  className=" rounded-2xl object-cover h-44 w-44 mt-6  lg:mt-16"
-                  alt="business logo"
-                  src={businessData.logoUrl}
-                />
-
-                <h1 className="text-center mb-2 mt-3">{businessData?.name}</h1>
-                <h5 className="text-slate-500 text-center">
-                  Thank you for visiting us!
-                </h5>
-              </Fade>
-              <Fade cascade delay={200}>
-                <div className="w-full flex justify-center">
-                  {stars.map((index) => {
-                    return (
-                      <div
-                        key={index}
-                        onMouseEnter={() => {
-                          setHoverRating(index);
-                        }}
-                        onMouseLeave={() => {
-                          setHoverRating(null);
-                        }}
-                        onClick={() => {
-                          setCurrentRating(index);
-                          gtag.event({
-                            action: `star rating: ${index}`,
-                            category: `${businessData?.id}`,
-                            label: `${businessData?.name}`,
-                            value: `${index}`,
-                          });
-                          if (index >= 4) {
-                            toGoogleReviews();
-                          } else {
-                            setScreen("form");
-                          }
-                        }}
-                        className="text-6xl sm:text-8xl lg:text-9xl"
-                      >
-                        {showStar(index) ? (
-                          <TiStar className=" text-amber-400 hover:scale-110 transition-all" />
-                        ) : (
-                          <TiStar className=" text-amber-100 hover:scale-110 transition-all" />
-                        )}
-                      </div>
-                    );
-                  })}
-                </div>
-              </Fade>
-              <Fade cascade delay={500}>
-                <h6 className="text-slate-500 mt-2 text-center">
-                  Please let us know how we did.
-                </h6>
-              </Fade>
+          <div className="absolute w-full items-center flex flex-col">
+            <img
+              className="rounded-2xl object-cover h-44 w-44 mt-6 lg:mt-16"
+              alt="business logo"
+              src={businessData.logoUrl}
+            />
+            <h1 className="text-center mb-2 mt-3">{businessData?.name}</h1>
+            <h5 className="text-slate-500 text-center">
+              Thank you for visiting us!
+            </h5>
+            <div className="w-full flex justify-center">
+              {stars.map((index) => {
+                return (
+                  <div
+                    key={index}
+                    onMouseEnter={() => {
+                      setHoverRating(index);
+                    }}
+                    onMouseLeave={() => {
+                      setHoverRating(null);
+                    }}
+                    onClick={() => {
+                      setCurrentRating(index);
+                      gtag.event({
+                        action: `star rating: ${index}`,
+                        category: `${businessData?.id}`,
+                        label: `${businessData?.name}`,
+                        value: `${index}`,
+                      });
+                      if (index >= 4) {
+                        toGoogleReviews();
+                      } else {
+                        setScreen("form");
+                      }
+                    }}
+                    className="text-6xl sm:text-8xl lg:text-9xl"
+                  >
+                    {showStar(index) ? (
+                      <TiStar className="text-amber-400 hover:scale-110 transition-all" />
+                    ) : (
+                      <TiStar className="text-amber-100 hover:scale-110 transition-all" />
+                    )}
+                  </div>
+                );
+              })}
             </div>
-          </Fade>
+            <h6 className="text-slate-500 mt-2 text-center">
+              Please let us know how we did.
+            </h6>
+          </div>
         )}
-        <Fade when={screen == "form"} collapse>
+        {screen == "form" && (
           <div className="w-full min-h-full flex flex-col">
-            <div className="flex justify-center ">
-              <div className=" flex relative w-full max-w-2xl">
+            <div className="flex justify-center">
+              <div className="flex relative w-full max-w-2xl">
                 <button
                   onClick={() => setScreen("init")}
-                  className="text-slate-500 mt-7  absolute left-5 hover:bg-slate-50 p-2 rounded-xl"
+                  className="text-slate-500 mt-7 absolute left-5 hover:bg-slate-50 p-2 rounded-xl"
                 >
                   <IoArrowBack />
                 </button>
@@ -146,9 +136,9 @@ function Index({ businessData = {} }) {
               </iframe>
             )}
           </div>
-        </Fade>
-        <Fade when={screen == "thankYou"} collapse>
-          <div className="w-full h-screen flex flex-col justify-center items-center   ">
+        )}
+        {screen == "thankYou" && (
+          <div className="w-full h-screen flex flex-col justify-center items-center">
             <h1 className="text-center mb-2 mt-3">
               Thank you for your feedback!
             </h1>
@@ -156,7 +146,7 @@ function Index({ businessData = {} }) {
               please leave us a review on Google.
             </h5>
           </div>
-        </Fade>
+        )}
       </div>
     );
   } else {
